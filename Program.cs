e0f5e70cd93656a865e972cf9286214102a9e966
ericjulander <jul15001@byui.edu>
2019-06-11 16:03:42 -0600

    Started playing around with the JOSN stuff

--- Program.cs
+++ Program.cs
@@ -1,19 +1,28 @@
 ﻿using System;
 using System.IO;
 using System.Threading.Tasks;
+using JSON_TO_CSV_CONVERTER;
 
 namespace air_nomads_canvas_to_CSV
 {
     class Program
     {
-        static async Task Main(string[] args)
-        {
+
+        public async static void makeHttpCall(){
+
             string pathToToken = @"C:\Users\lwargha\auth.txt";
             string url = "https://byui.instructure.com/api/v1/courses/47002/quizzes/585539/questions";
             // string path = args[0];
             string text = System.IO.File.ReadAllText(pathToToken);
             var result = await HTTPHelper.MakeHttpAuthCall(text, url);
             System.Console.WriteLine(result);
+
+        }
+        static void Main(string[] args)
+        {
+            var JSON = System.IO.File.ReadAllText("./test.json");
+            Console.WriteLine(JSON);
+            JSON_TO_CSV.getCSVFromJSON(JSON);
         }
     }
 }
