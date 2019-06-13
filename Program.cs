8b6b5f14bafda0ec9f1a99a8a0e4c956dbbcf06e
John Reiley <rei15006@byui.edu>
2019-06-13 16:03:03 -0600

    Changed the program to grab token from env vars and manual input for api endpoint

--- Program.cs
+++ Program.cs
@@ -11,16 +11,34 @@ namespace air_nomads_canvas_to_CSV
     {
         static async Task Main(string[] args)
         {
-            string token = args[0];
-            string url = "https://byui.instructure.com/api/v1/courses/47002/quizzes/585539/questions";
+            string token = Environment.GetEnvironmentVariable("API_TOKEN");
+            string url = promtUrlEnpoint();
+            string filename = promtFilename();
+
+            System.Console.WriteLine("URL: " + url);
+
+            // string url = "https://byui.instructure.com/api/v1/courses/47002/quizzes/585539/questions";
+
             var result = await HTTPHelper.MakeHttpAuthCall(token, url);
             var quizzez = JsonToCsv.convertJsonToQuizList(result);
-        
-            using (var writer = new StreamWriter("quiz.csv"))
+
+            using (var writer = new StreamWriter(filename))
             using (var csv = new CsvWriter(writer))
             {
                 csv.WriteRecords(quizzez);
             }
+        }
+
+        static string promtUrlEnpoint()
+        {
+            System.Console.WriteLine("Enter the api endpoint:");
+            return ("https://byui.instructure.com/" + Console.ReadLine());
+        }
+
+        static string promtFilename()
+        {
+            System.Console.WriteLine("Enter destination filename:");
+            return Console.ReadLine();
         }
     }
 }
