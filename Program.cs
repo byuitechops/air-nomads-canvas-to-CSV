154a358be67c4d27aa4611e7ae8e1cd416773a54
ericjulander <jul15001@byui.edu>
2019-06-13 16:04:36 -0600

    Program now outputs a string rather than a csvfile it is also more dynamic in depositing the json into objects

--- Program.cs
+++ Program.cs
@@ -9,18 +9,22 @@ namespace air_nomads_canvas_to_CSV
 {
     class Program
     {
+
+
         static async Task Main(string[] args)
         {
             string token = args[0];
             string url = "https://byui.instructure.com/api/v1/courses/47002/quizzes/585539/questions";
             var result = await HTTPHelper.MakeHttpAuthCall(token, url);
-            var quizzez = JsonToCsv.convertJsonToQuizList(result);
-        
-            using (var writer = new StreamWriter("quiz.csv"))
+            var quizzez = JsonToCanvas<Quiz>.convertJsonToQuizList(result);
+            var csvString = "";
+            using (var writer = new StringWriter())
             using (var csv = new CsvWriter(writer))
             {
                 csv.WriteRecords(quizzez);
-            }
+                csvString =  (writer.ToString());
+            }    
+            System.Console.WriteLine(csvString);
         }
     }
 }
