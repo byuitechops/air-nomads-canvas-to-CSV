f3480c42d9731859e0c8c80915fcb1d0e2e4ac63
ericjulander <jul15001@byui.edu>
2019-06-17 08:42:43 -0600

    Changed the class to work using LINQ. Also updated the class name to be more universal.

--- CanvasToCsvConverter.cs
+++ CanvasToCsvConverter.cs
@@ -5,47 +5,39 @@ using System.Collections.Generic;
 using System.IO;
 using System;
 
-namespace CanvasToCsvConverter
+namespace ClassToCsvConverter
 {
-    public static class CanvasToCsv{
+    public static class ClassToCsv{
 
         /*
          * Creates a new object only containing values which mactch the headers
          */
         public static dynamic newObjectFromHeaders<T>(T oldObject, string[] headers){
             dynamic newObject = new ExpandoObject();
-            var dictionary = (IDictionary<string, object>)newObject;
-            foreach(var field in oldObject.GetType().GetProperties()){
-                if(Array.Exists(headers, element => element == field.Name))
-                    dictionary.Add(field.Name, field.GetValue(oldObject));
-            }
+            var dictionary = (IDictionary<string, object>) newObject;
+            oldObject.GetType().GetProperties()
+                     .Where(property => headers.Any(selectorz => property.Name == selectorz)).ToList()
+                     .ForEach(property => dictionary.Add(property.Name, property.GetValue(oldObject)));
             return newObject;
         }
         
         /*
          * This creates a csv string with only containing fields under the headers mentioned in the array
          */
-        public static string convertToCSV<T>(List<T> CanvasObjectList, string[] headers){
-            string csvOutput = "";
-            var newCanvasObjectList = CanvasObjectList.Select<T, dynamic>( (canvasObject) => {return newObjectFromHeaders(canvasObject, headers);});
-            using (var writer = new StringWriter())
-            using (var csv = new CsvWriter(writer))
-            {
-                csv.WriteRecords(newCanvasObjectList);
-                csvOutput =  (writer.ToString());
-            }    
-            return csvOutput;
+        public static string convertToCSV<T>(List<T> ClassObjectList, string[] headers){
+            var newCanvasObjectList = ClassObjectList.Select<T, dynamic>( (canvasObject) => {return newObjectFromHeaders(canvasObject, headers);}).ToList();
+            return convertToCSV<dynamic>(newCanvasObjectList);
         }
 
         /*
          * When no headers are specified, it creates a csv file for all headers
          */
-        public static string convertToCSV<T>(List<T> CanvasObject){
+        public static string convertToCSV<T>(List<T> ClassObjectList){
             string csvOutput = "";
             using (var writer = new StringWriter())
             using (var csv = new CsvWriter(writer))
             {
-                csv.WriteRecords(CanvasObject);
+                csv.WriteRecords(ClassObjectList);
                 csvOutput =  (writer.ToString());
             }    
             return csvOutput;
--- Program.cs
+++ Program.cs
@@ -2,7 +2,9 @@
 using System.Threading.Tasks;
 using JsonConverter;
 using CanvasObjects;
-using CanvasToCsvConverter;
+using ClassToCsvConverter;
+using System.Dynamic;
+using System.Collections.Generic;
 
 namespace air_nomads_canvas_to_CSV
 {
@@ -11,11 +13,12 @@ namespace air_nomads_canvas_to_CSV
 
         static async Task Main(string[] args)
         {
-            string token = args[0];
-            string url = "https://byui.instructure.com/api/v1/courses/47002/quizzes/585539/questions";
-            var result = await HTTPHelper.MakeHttpAuthCall(token, url);
-            var quizzez = JsonToCanvas<Quiz>.convertJsonToQuizList(result);
-            var csvString = CanvasToCsv.convertToCSV<Quiz>(quizzez, new string[]{"id", "question_name", "incorrect_comments"});
+
+            // string token = args[0];
+            // string url = "https://byui.instructure.com/api/v1/courses/47002/quizzes/585539/questions";
+            // var result = await HTTPHelper.MakeHttpAuthCall(token, url);
+            var quizzez = JsonToCanvas<Quiz>.convertJsonToQuizList(System.IO.File.ReadAllText("./test_quiz.json"));
+            var csvString = ClassToCsv.convertToCSV<Quiz>(quizzez, new string[]{"id", "question_name", "incorrect_comments"});
             System.IO.File.WriteAllText("./filtered_output.csv", csvString);
         }
     }
